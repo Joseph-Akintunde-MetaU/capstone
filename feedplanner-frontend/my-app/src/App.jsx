444de21b6058d3ef322acdd3c48edc290d45f982
Joseph Akintunde <joseph1of1@meta.com>
2025-06-26 11:25:11 -0700

    fixing git conflicts

--- feedplanner-frontend/my-app/src/App.jsx
+++ feedplanner-frontend/my-app/src/App.jsx
@@ -1,11 +1,25 @@
+import { Routes, Route, useNavigate} from 'react-router-dom'
 import './App.css'
 import { UserAuthPage } from './UserAuthPage'
+import { auth } from './config/firebase.config'
+import { signOut } from 'firebase/auth'
+import { HomePage } from './HomePage'
 function App() {
-
+  const nav = useNavigate()
+   function isSignedOut(){
+        signOut(auth).then(() => {
+            console.log("Signed out Successfully")
+            nav("/")
+        }).catch((error) => {
+            console.error(error)
+        });
+    }
   return (
-      <div>  
-        <UserAuthPage/>
-      </div>
-  )
+      <Routes>
+        <Route path='/' element={<UserAuthPage />} />
+        <Route path='/home' element={<HomePage isSignedOut={isSignedOut}/>} />
+      </Routes>
+  );
 }
-export default App
+
+export default App;
