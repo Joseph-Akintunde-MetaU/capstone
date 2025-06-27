12d9d2bb6381038980f76c2e2b7a65a545318b99
Joseph Akintunde <joseph1of1@meta.com>
2025-06-27 09:48:58 -0700

    auth and jwt token verification. also changed the functions format to a format i'm used to. learned the previous one from a tutorial and got to understand it.

--- feedplanner-backend/functions/index.js
+++ feedplanner-backend/functions/index.js
@@ -8,14 +8,16 @@ const db = admin.firestore();
 //creating a new cloud function that's triggered by an https request.
 exports.validateUserJWTToken = functions.https.onRequest(async (req,res) => {
     cors(req,res, async () => {
+        //authorization - bearer token to ensure authentication
         const authorizationHeader = req.get("Authorization")
         if(!authorizationHeader || !authorizationHeader.startsWith('Bearer ')){
             return res.status(401).json({error: "Unauthorized"})
         }
         const token = authorizationHeader.split('Bearer ')[1]
         try{
+            //verify the token
             const decodedToken = await admin.auth().verifyIdToken(token)
-            //this adds the token and its details to a collection called users in my database.
+            //and after that, retrieves user data with unique uid. if the user doesnt exist, this adds the token and its details to a collection called users in my database.
             if(decodedToken){
                 const docRef = db.collection("users").doc(decodedToken.uid)
                 const doc = await docRef.get()
