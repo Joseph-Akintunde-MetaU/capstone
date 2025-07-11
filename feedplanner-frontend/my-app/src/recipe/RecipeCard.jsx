febe0ad7c7255a7fbe8039e4718207f221ce5c79
Joseph Akintunde <joseph1of1@meta.com>
2025-07-11 12:27:44 -0700

    connected backend POST route so now when i click on the toggle, i see the recipe id in my database and when i click again, it deletes as it should

--- feedplanner-frontend/my-app/src/recipe/RecipeCard.jsx
+++ feedplanner-frontend/my-app/src/recipe/RecipeCard.jsx
@@ -2,21 +2,35 @@ import AddToMealPlan from "./AddToMealPlan"
 import "./RecipeCard.css"
 import { getMealPlans } from "../utility/getmealplanner"
 import {FaStar} from "react-icons/fa"
+import { auth } from "../config/firebase.config"
 import { useState,useEffect } from "react"
 export function RecipeCard({id, image, title, duration}){
     const [openModal, setOpenModal] = useState(false)
     const [rating, setRating] = useState(null)
     const [hover, setHover] = useState(null)
-    const bookmarked = `bookmarked: ${id}`
-    const [bookmarkedRecipe, setBookmarkedRecipe] = useState(true)
-    useEffect(() => {
-        const getBookmarkedStatus = localStorage.getItem(bookmarked)
-        if(getBookmarkedStatus === 'true') setBookmarkedRecipe(true)
-    }, [bookmarked])
-    function bookMarking(){
-        const notBookmarked = !bookmarkedRecipe;
-        setBookmarkedRecipe(notBookmarked)
-        localStorage.setItem(bookmarked, notBookmarked.toString())
+    const [bookmarkedRecipe, setBookmarkedRecipe] = useState(false)
+    async function bookMarkingToggle(e){
+        e.stopPropagation()
+       
+        try{
+            const token = await auth.currentUser.getIdToken()
+            const nextBookmarkedState = !bookmarkedRecipe;
+            await fetch("http://localhost:5001/feedplanner/us-central1/api/bookmark",{
+                method: "POST",
+                headers:{
+                    Authorization: `Bearer ${token}`,
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    recipeId: id,
+                    isBookmarked: nextBookmarkedState,
+                }),
+            });
+            setBookmarkedRecipe(nextBookmarkedState)
+            localStorage.setItem('bookmarked', nextBookmarkedState.toString())
+        }catch(error){
+            console.error("failed to bookmark")
+        }
     }
      return(
         <div>
@@ -46,11 +60,8 @@ export function RecipeCard({id, image, title, duration}){
                         setOpenModal(true)
                     }}
                     >ADD TO MEAL PLANNER</button> 
-                    <button onClick={(e) => {
-                        bookMarking()
-                        e.stopPropagation()
-                    }}>
-                       <img src={ bookmarkedRecipe ? "https://img.icons8.com/?size=100&id=25157&format=png&color=000000" : "https://img.icons8.com/?size=100&id=26083&format=png&color=000000"}/>
+                    <button onClick={bookMarkingToggle}>
+                       <img src={ bookmarkedRecipe ? "https://img.icons8.com/?size=100&id=26083&format=png&color=000000" : "https://img.icons8.com/?size=100&id=25157&format=png&color=000000"}/>
                     </button>
                     </div>  
                 </div>
