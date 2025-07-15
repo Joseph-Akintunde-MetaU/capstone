386d8a089202a92da9466cd17c3efe2678b16b6d
Joseph Akintunde <joseph1of1@meta.com>
2025-07-15 11:50:59 -0700

    removed hardcoded api keys for debugging

--- feedplanner-frontend/my-app/src/recipe/recipePage.jsx
+++ feedplanner-frontend/my-app/src/recipe/recipePage.jsx
@@ -6,10 +6,9 @@ import { auth } from "../config/firebase.config"
 import { onAuthStateChanged } from "firebase/auth"
 import CircularProgress from '@mui/material/CircularProgress';
 import { useEffect } from "react";
-export function RecipePage(){
+export function RecipePage({recipes, setRecipes}){
     const [loading, setLoading] = useState(true)
-    const [recipes, setRecipes] = useState ([]);
-    const apiKey = `995c9d32eea04be99d91f6c9dbe6b421`
+    const apiKey = process.env.REACT_APP_API_KEY
         async function getRecipes(){
         const fetchMatchingRecipe = onAuthStateChanged(auth, async(user) => {
             try{
