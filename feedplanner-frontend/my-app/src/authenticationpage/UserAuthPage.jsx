6a89ecbc5975ae8abc71153dd084fb7e2909dccd
Joseph Akintunde <joseph1of1@meta.com>
2025-07-16 11:08:48 -0700

    removed debugging console.logs

--- feedplanner-frontend/my-app/src/authenticationpage/UserAuthPage.jsx
+++ feedplanner-frontend/my-app/src/authenticationpage/UserAuthPage.jsx
@@ -93,7 +93,6 @@ export function UserAuthPage(){
             try{
                 const user = userCredential.user;
                 const token = await user.getIdToken()
-                console.log(token)
                 const response = await fetch(
                     baseUrl,
                     {
